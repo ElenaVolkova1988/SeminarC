51f3664642fe425758e28ffc9d4c7b4d3203c628
Elena <xoroshj_chelovek@mail.ru>
2022-10-01 16:59:29 +0300

    Задача1 выполнена

---
+++ Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Console.WriteLine("Input a first number");
+int n1= Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine("Input a second number");
+int n2= Convert.ToInt32 (Console.ReadLine ());
+
+int quad2 =n2*n2;
+
+if (n1== quad2)
+{
+     Console.WriteLine ("Yes");
+}
+else
+{
+    Console.WriteLine ("No");
+}
+*/
